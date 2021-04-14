480fb9057f51eebe7b1def0f3229135c18f9c1f0
gen.ryongfa <ryonghwa.om@gmail.com>
2021-04-14 12:31:47 +0000

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
   def index
-    
   end
 
   def show
     @user = User.find(params[:id])
-    @photos = Photo.order(id: :desc).page(params[:page]).per(25)
+    # @photos = Photo.order(id: :desc).page(params[:page]).per(25)
   end
 
   def new
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to :index
+      redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
