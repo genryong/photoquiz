3af00134706ebe08bb1b1279c867a5974b395807
gen.ryongfa <ryonghwa.om@gmail.com>
2021-05-31 08:42:06 +0000

    for heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,22 +7,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   
   if Rails.env.production?
-      include Cloudinary::CarrierWave
-    else
-      storage :file
+    include Cloudinary::CarrierWave
+  else
+    storage :file
   end
   
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-end
-
-　
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
@@ -56,5 +52,5 @@ end
   # def filename
   #   "something.jpg" if original_filename
   # end
-
+end
 
